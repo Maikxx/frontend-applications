b7cdf62a19ad7f42009fb59b58421ae7685096eb
Maikel van Veen <maikeloo10@hotmail.com>
2018-10-10 18:33:18 +0200

    refactor(*): improve code style

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import '@polymer/app-route/app-route.js'
 import '@polymer/iron-pages/iron-pages.js'
 import '@polymer/iron-selector/iron-selector.js'
 import '@polymer/paper-icon-button/paper-icon-button.js'
-import './components/ta-icons.js'
+import './components/ri-icons.js'
 
 // Gesture events like tap and track generated from touch will not be
 // preventable, allowing for better scrolling performance.
@@ -22,137 +22,146 @@ setPassiveTouchGestures(true)
 setRootPath(MyAppGlobals.rootPath)
 
 class MyApp extends PolymerElement {
-  static get template() {
-    return html`
-      <style>
-        :host {
-          --app-primary-color: #4285f4;
-          --app-secondary-color: black;
+    static get template() {
+        return html`
+            <style>
+                :host {
+                    --app-primary-color: #4285f4;
+                    --app-secondary-color: black;
 
-          display: block;
-        }
+                    display: block;
+                }
 
-        app-drawer-layout:not([narrow]) [drawer-toggle] {
-          display: none;
-        }
+                app-drawer-layout:not([narrow]) [drawer-toggle] {
+                    display: none;
+                }
 
-        app-header {
-          color: #fff;
-          background-color: var(--app-primary-color);
-        }
+                app-header {
+                    color: #fff;
+                    background-color: var(--app-primary-color);
+                }
 
-        app-header paper-icon-button {
-          --paper-icon-button-ink-color: white;
-        }
+                app-header paper-icon-button {
+                    --paper-icon-button-ink-color: white;
+                }
 
-        .drawer-list {
-          margin: 0 20px;
-        }
+                .drawer-list {
+                margin: 0 20px;
+                }
 
-        .drawer-list a {
-          display: block;
-          padding: 0 16px;
-          text-decoration: none;
-          color: var(--app-secondary-color);
-          line-height: 40px;
-        }
+                .drawer-list a {
+                    display: block;
+                    padding: 0 16px;
+                    text-decoration: none;
+                    color: var(--app-secondary-color);
+                    line-height: 40px;
+                }
 
-        .drawer-list a.iron-selected {
-          color: black;
-          font-weight: bold;
-        }
-      </style>
+                .drawer-list a.iron-selected {
+                    color: black;
+                    font-weight: bold;
+                }
+            </style>
 
-      <app-location route="{{route}}" url-space-regex="^[[rootPath]]">
-      </app-location>
+            <app-location route="{{route}}" url-space-regex="^[[rootPath]]">
+            </app-location>
 
-      <app-route route="{{route}}" pattern="[[rootPath]]:page" data="{{routeData}}" tail="{{subroute}}">
-      </app-route>
+            <app-route route="{{route}}" pattern="[[rootPath]]:page" data="{{routeData}}" tail="{{subroute}}">
+            </app-route>
 
-      <app-drawer-layout fullbleed="" narrow="{{narrow}}">
-        <!-- Drawer content -->
-        <app-drawer id="drawer" slot="drawer" swipe-open="[[narrow]]">
-          <app-toolbar>Menu</app-toolbar>
-          <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
-            <a name="home" href="[[rootPath]]home">Home</a>
-            <a name="view2" href="[[rootPath]]view2">View Two</a>
-          </iron-selector>
-        </app-drawer>
+            <app-drawer-layout fullbleed="" narrow="{{narrow}}">
+                <!-- Drawer content -->
+                <app-drawer id="drawer" slot="drawer" swipe-open="[[narrow]]">
+                    <app-toolbar>
+                        Menu
+                    </app-toolbar>
 
-        <!-- Main content -->
-        <app-header-layout has-scrolling-region="">
+                    <iron-selector selected="[[page]]" attr-for-selected="name" class="drawer-list" role="navigation">
+                        <a name="home" href="[[rootPath]]home">
+                            Home
+                        </a>
+                        <a name="view2" href="[[rootPath]]view2">
+                            View Two
+                        </a>
+                    </iron-selector>
+                </app-drawer>
 
-          <app-header slot="header" condenses="" reveals="" effects="waterfall">
-            <app-toolbar>
-              <paper-icon-button icon="my-icons:menu" drawer-toggle=""></paper-icon-button>
-              <div main-title="">My App</div>
-            </app-toolbar>
-          </app-header>
+                <!-- Main content -->
+                <app-header-layout has-scrolling-region="">
 
-          <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
-            <ta-home name="home"></ta-home>
-            <my-view2 name="view2"></my-view2>
-            <my-view404 name="view404"></my-view404>
-          </iron-pages>
-        </app-header-layout>
-      </app-drawer-layout>
-    `;
-  }
+                <app-header slot="header" condenses="" reveals="" effects="waterfall">
+                    <app-toolbar>
+                        <paper-icon-button icon="my-icons:menu" drawer-toggle=""></paper-icon-button>
+                        <div main-title="">
+                            Jeugdzorg Risico Indicatie Applicatie
+                        </div>
+                    </app-toolbar>
+                </app-header>
 
-  static get properties() {
-    return {
-      page: {
-        type: String,
-        reflectToAttribute: true,
-        observer: '_pageChanged'
-      },
-      routeData: Object,
-      subroute: Object
-    }
-  }
-
-  static get observers() {
-    return [
-      '_routePageChanged(routeData.page)'
-    ]
-  }
-
-  _routePageChanged(page) {
-     // Show the corresponding page according to the route.
-     //
-     // If no page was found in the route data, page will be an empty string.
-     // Show 'home' in that case. And if the page doesn't exist, show 'view404'.
-    if (!page) {
-      this.page = 'home'
-    } else if (['home', 'view2'].indexOf(page) !== -1) {
-      this.page = page
-    } else {
-      this.page = 'view404'
+                <iron-pages selected="[[page]]" attr-for-selected="name" role="main">
+                    <ri-home name="home"></ri-home>
+                    <my-view2 name="view2"></my-view2>
+                    <ri-notFound name="notFound"></ri-notFound>
+                </iron-pages>
+                </app-header-layout>
+            </app-drawer-layout>
+        `
     }
 
-    // Close a non-persistent drawer when the page & route are changed.
-    if (!this.$.drawer.persistent) {
-      this.$.drawer.close()
+    static get properties() {
+        return {
+            page: {
+                type: String,
+                reflectToAttribute: true,
+                observer: '_pageChanged'
+            },
+            routeData: Object,
+            subroute: Object
+        }
     }
-  }
 
-  _pageChanged(page) {
-    // Import the page component on demand.
-    //
-    // Note: `polymer build` doesn't like string concatenation in the import
-    // statement, so break it up.
-    switch (page) {
-      case 'home':
-        import('./views/ta-home.js')
-        break
-      case 'view2':
-        import('./my-view2.js')
-        break
-      case 'view404':
-        import('./my-view404.js')
-        break
+    static get observers() {
+        return [
+            '_routePageChanged(routeData.page)'
+        ]
     }
-  }
+
+    _routePageChanged(page) {
+        // Show the corresponding page according to the route.
+        //
+        // If no page was found in the route data, page will be an empty string.
+        // Show 'home' in that case. And if the page doesn't exist, show 'notFound'.
+        if (!page) {
+            this.page = 'home'
+        } else if (['home', 'view2'].indexOf(page) !== -1) {
+            this.page = page
+        } else {
+            this.page = 'notFound'
+        }
+
+        // Close a non-persistent drawer when the page & route are changed.
+        if (!this.$.drawer.persistent) {
+            this.$.drawer.close()
+        }
+    }
+
+    _pageChanged(page) {
+        // Import the page component on demand.
+        //
+        // Note: `polymer build` doesn't like string concatenation in the import
+        // statement, so break it up.
+        switch (page) {
+            case 'home':
+                import('./views/ri-home.js')
+                break
+            case 'view2':
+                import('./my-view2.js')
+                break
+            case 'notFound':
+                import('./views/ri-notFound.js')
+                break
+        }
+    }
 }
 
 window.customElements.define('my-app', MyApp)
