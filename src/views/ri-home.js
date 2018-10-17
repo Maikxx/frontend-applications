85b4cc2d353bfe3806fd735ac0482462084238b9
Maikel van Veen <maikeloo10@hotmail.com>
2018-10-17 11:10:04 +0200

    feat(home): add reset button

--- src/views/ri-home.js
+++ src/views/ri-home.js
@@ -5,6 +5,10 @@ class HomeView extends PolymerElement {
     static get template() {
         return html`
             <style include="shared-styles">
+                :host {
+                    --app-tertiary-color: #698996;
+                }
+
                 section.section {
                     padding: 0 36px;
                 }
@@ -38,6 +42,38 @@ class HomeView extends PolymerElement {
                     display: block;
                     margin-top: 12px;
                     font-weight: bold;
+                }
+
+                button {
+                    display: inline-block;
+                    box-sizing: border-box;
+                    border: none;
+                    border-radius: 4px;
+                    padding: 0 12px;
+                    min-width: 64px;
+                    height: 36px;
+                    vertical-align: middle;
+                    text-align: center;
+                    text-overflow: ellipsis;
+                    color: #fff;
+                    background-color: var(--app-tertiary-color);
+                    box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+                    font-size: 14px;
+                    font-weight: 500;
+                    line-height: 36px;
+                    overflow: hidden;
+                    outline: none;
+                    cursor: pointer;
+                    transition: box-shadow 0.2s;
+                }
+
+                button:hover,
+                button:focus {
+                    box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+                }
+
+                button:active {
+                    box-shadow: 0px 5px 5px -3px rgba(0, 0, 0, 0.2), 0px 8px 10px 1px rgba(0, 0, 0, 0.14), 0px 3px 14px 2px rgba(0, 0, 0, 0.12);
                 }
             </style>
 
@@ -75,9 +111,20 @@ class HomeView extends PolymerElement {
                             N.b. ga altijd uit van uw eigen verstand en gebruik deze tool enkel als hulpmiddel.
                         </span>
                     </p>
+
+                    <footer>
+                        <button type="button" on-click="onReset">
+                            Reset alle velden
+                        </button>
+                    </footer>
                 </section>
             </div>
         `
+    }
+
+    onReset() {
+        window.localStorage.clear()
+        location.reload()
     }
 }
 
